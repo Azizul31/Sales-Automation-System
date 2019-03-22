c8157be14b1e1571fd738fab7fdb950eda59f0af
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-03-23 04:46:34 +0600

    login page modified and added in php format

--- login page final/Contact_us.php
+++ login page final/Contact_us.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    </head>
-<body>
 
 <?php
 include("head.php");
 include("foot.php");
 ?>
 
-</body>
---
+++ login page final/login.php
@@ -0,0 +1,44 @@
+
+
+<?php
+include("head.php");
+?>
+<div class="limiter">
+
+		<div class="container-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title">
+                    <h1>Samah Razor Blades Ind. Limited</h1>   <br>
+						Member Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+<?php
+include("foot.php");
+?>
+
+
