6f7d59edb3774e7ec6596ce9e71eff3edf686cac
Ashik549 <ashikur.rahman38@outlook.com>
2019-03-30 22:56:07 +0600

    Initial login page which is able to check from multiple tables committed.

--- login page final/loginform.inc.php
+++ login page final/loginform.inc.php
@@ -1,32 +1,48 @@
 <?php
+function query ($table, $username, $password, $link) {
+    $query = "SELECT ID, Password 
+                    FROM $table
+                    WHERE ID='$username' AND Password='$password'";
+    
+    if ($query_run = mysqli_query ($link, $query)) {
+            $query_num_rows = mysqli_num_rows ($query_run);
+            echo $query_num_rows;
+            
+            if ($query_num_rows == 0) {
+                return 0;
+            }
+            else if ($query_num_rows == 1) {
+                return 1;
+                /*$user_id = mysqli_result ($query_run, 0, 'id');
+                $_SESSION ['user_id'] = $user_id;
+                header ('Location: index.php');*/
+            }
+        }
+}
 
 if (isset($_POST ['username']) && isset ($_POST ['password'])) {
     $username = $_POST ['username'];
     $password = $_POST ['password'];
     
     $password_hash = md5($password);
-    
+    $exec = 'executives';
+    $exec = mysqli_real_escape_string ($link, $exec);
+    $reg = 'regional managers';
+    $reg = mysqli_real_escape_string ($link, $reg);
+    $ter = 'territory managers';
+    $ter = mysqli_real_escape_string ($link, $ter);
+    $sal = 'salesmen';
+    $sal = mysqli_real_escape_string ($link, $sal);
     if (!empty ($username) && !empty($password)) {
         
-        $query = "SELECT ID, Password 
-                    FROM executives
-                    WHERE ID='$username' AND Password='$password_hash'"; 
+        if (query ($exec, $username, $password_hash, $link)) echo 'exec ok';
+        else if (query ($reg, $username, $password_hash, $link)) echo 'reg ok';
+        else if (query ($ter, $username, $password_hash, $link)) echo 'ter ok';
+        else if (query ($sal, $username, $password_hash, $link)) echo 'sal ok';
+        else echo 'hoy nai';
         
-        if ($query_run = mysqli_query ($link, $query)) {
-            $query_num_rows = mysqli_num_rows ($query_run);
-            echo $query_num_rows;
-            
-            if ($query_num_rows == 0) {
-                echo 'Invalid username/password combo';
-            }
-            else if ($query_num_rows == 1) {
-                echo 'OK Bitch!';
-                /*$user_id = mysqli_result ($query_run, 0, 'id');
-                $_SESSION ['user_id'] = $user_id;
-                header ('Location: index.php');*/
-            }
-        }
-        else echo 'bokachoda';
+        
+        
     }
     else echo 'You must supply a username and password.';
 }
