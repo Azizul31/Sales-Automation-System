d0abe2774171d91910a6f5cd18f8cad39c32cc83
Azizul Hakim Chowdhury <azizul.cuet.cse.31@gmail.com>
2019-03-31 00:40:07 +0600

    log in front end+back end initial done

--- login page final/login.php
+++ login page final/login.php
@@ -1,19 +1,106 @@
-
-
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["Name"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["Name"]);
+    }
+    
+    // Check if password is empty
+    if(empty(trim($_POST["Password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["Password"]);
+    }
+    
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT ID, Name, Password FROM salesmen WHERE Name = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = $username;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Store result
+                mysqli_stmt_store_result($stmt);
+                
+                // Check if username exists, if yes then verify password
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                    // Bind result variables
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["ID"] = $id;
+                            $_SESSION["Name"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
 <?php
 include("head.php");
 ?>
 <div class="limiter">
 
 		<div class="container-login100">
-				<form class="login100-form validate-form">
+				<form class="login100-form validate-form" action= "" method="POST">
 					<span class="login100-form-title">
                     <h1>Samah Razor Blades Ind. Limited</h1>   <br>
 						Member Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="username" placeholder="Username">
+						<input class="input100" type="text" name="Name" placeholder="Username">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-envelope" aria-hidden="true"></i>
@@ -21,7 +108,7 @@ include("head.php");
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="Password" name="Password" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
@@ -29,15 +116,14 @@ include("head.php");
 					</div>
 					
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
-						</button>
+						<input type="submit" value="Login">
 					</div>
 				</form>
 			</div>
 		</div>
 	</div>
 <?php
+
 include("foot.php");
 ?>
 
--- login page final/loginform.inc.php
+++ login page final/loginform.inc.php
@@ -9,8 +9,8 @@ if (isset($_POST ['username']) && isset ($_POST ['password'])) {
     if (!empty ($username) && !empty($password)) {
         
         $query = "SELECT ID, Password 
-                    FROM executives
-                    WHERE ID='$username' AND Password='$password_hash'"; 
+                    FROM salesmen
+                    WHERE Name='$username' AND Password='$password_hash'"; 
         
         if ($query_run = mysqli_query ($link, $query)) {
             $query_num_rows = mysqli_num_rows ($query_run);
@@ -20,22 +20,52 @@ if (isset($_POST ['username']) && isset ($_POST ['password'])) {
                 echo 'Invalid username/password combo';
             }
             else if ($query_num_rows == 1) {
-                echo 'OK Bitch!';
+                header("location: dataentry.php");
+    exit;
                 /*$user_id = mysqli_result ($query_run, 0, 'id');
                 $_SESSION ['user_id'] = $user_id;
                 header ('Location: index.php');*/
             }
         }
-        else echo 'bokachoda';
     }
     else echo 'You must supply a username and password.';
 }
-else echo '<br>vog';
+include("head.php");
 
 ?>
 
-<form action = "index.php" method = "POST">
-    <input type="text" name="username"><br>
-    <input type="password" name="password"><br>
-    <input type="submit" value="Submit">
-</form>
+<div class="limiter">
+
+		<div class="container-login100">
+				<form class="login100-form validate-form" action="index.php" method="POST">
+					<span class="login100-form-title">
+                    <h1>Samah Razor Blades Ind. Limited</h1>   <br>
+						Member Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="username" placeholder="Username" >
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<input type="submit" class="login100-form-btn" value="Submit"  >
+							
+					</div>
+				</form>
+			</div>
+		</div>
+<?php
+include("foot.php");
+?>
